6f69aea7bef753dd3ae71fff9f2458aabe3cec55
Anton Martynov <ntn@ntn-Inspiron-15-7000-Gaming.(none)>
2019-05-23 17:23:21 +0300

    # Оптимизация на месте

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ ENV DEBIAN_FRONTEND 'noninteractive'
 RUN apt-get update -y
 RUN apt-get install -y --no-install-recommends apt-utils
 
-RUN apt-get install -y wget vim
+RUN apt-get install -y wgetdocker system prune --volumes
 RUN apt-get install -y git
 
 RUN wget https://dl.google.com/go/go1.12.5.linux-amd64.tar.gz
--- agregator/CreateIndex.go
+++ agregator/CreateIndex.go
@@ -13,17 +13,17 @@ DROP INDEX IF EXISTS thread_forum_index;
 
 
 
---CREATE INDEX IF NOT EXISTS flat_sort ON post(thread_id, id);
+CREATE INDEX IF NOT EXISTS flat_sort ON post(thread_id, id);
 
 CREATE INDEX IF NOT EXISTS tree_sort ON post(thread_id, path, id);
 
---CREATE INDEX IF NOT EXISTS parent_tree_sort ON post (thread_id, substring(path from 1 for 6) desc, id);
+CREATE INDEX IF NOT EXISTS parent_tree_sort ON post (thread_id, substring(path from 1 for 6) desc, id);
 
 CREATE INDEX IF NOT EXISTS thread_created ON thread(forum, created);
 
 CREATE INDEX IF NOT EXISTS thread_forum_index ON thread (forum);
 
-CREATE INDEX IF NOT EXISTS thread_vote ON thread (id, vote);
+--CREATE INDEX IF NOT EXISTS thread_vote ON thread (id, vote);
 
 
 `
