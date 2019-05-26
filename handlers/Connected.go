9ae353c25215aac7ee5d799ce537408868c15393
Anton Martynov <ntn@ntn-Inspiron-15-7000-Gaming.(none)>
2019-05-26 12:22:44 +0300

    # Оптимизация на месте

--- handlers/Connected.go
+++ handlers/Connected.go
@@ -1,15 +1,14 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) Connected(c *gin.Context) {
-	err := h.Agregator.Clastering()
-	fmt.Println(err)
-	if err != nil {
-		c.JSON(409, "problems")
-	}
+	//err := h.Agregator.Clastering()
+	//fmt.Println(err)
+	//if err != nil {
+	//	c.JSON(409, "problems")
+	//}
 	c.JSON(201, "connected")
 }
